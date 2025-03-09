336d06fe948a9ff3129fdc3a18a8da014b55671a
Abhishek Keshri <iam2kabhishek@gmail.com>
2025-03-09 10:28:30 +0530

    feat: update group plugin defaults

--- plugins/group.sh
+++ plugins/group.sh
@@ -6,22 +6,17 @@ current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$current_dir/../lib/utils.sh"
 
 main() {
-    scripts=$(get_tmux_option "@tmux2k-group-plugins" "cpu ram cpu-temp uptime")
-    delimiter=$(get_tmux_option "@tmux2k-group-delimiter" "󰇝")
-	val=""
-	for script in $scripts
-	do
-		sname="${current_dir}/${script}.sh"
-		val+=$($sname)
-		if [ "$delimiter" = "" ]; then
-			val+=" "
-		else
-			val+=$delimiter
-		fi
-	done
-	size=${#val}
-	let "size = size-1"
-	val=${val:0:size}
+    scripts=$(get_tmux_option "@tmux2k-group-plugins" "cpu ram uptime")
+    delimiter=$(get_tmux_option "@tmux2k-group-delimiter" "")
+    val=""
+    for script in $scripts; do
+        sname="${current_dir}/${script}.sh"
+        val+="$($sname)"
+        val+=$delimiter
+    done
+    size=${#val}
+    let "size = size-1"
+    val=${val:0:size}
     echo -n "$val"
 }
 
