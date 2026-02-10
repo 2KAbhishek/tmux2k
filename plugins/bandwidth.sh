048ec1f7edefaa1c88c9121772fc8c141b43d50b
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-02-10 14:51:44 +0530

    fix: enhancements to bandwidth plugin

--- plugins/bandwidth.sh
+++ plugins/bandwidth.sh
@@ -4,7 +4,7 @@ current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$current_dir/../lib/utils.sh"
 
 if [[ $(uname -s) == "Darwin" ]]; then
-    network_name=$(get_tmux_option "@tmux2k-bandwidth-network-name" "en1")
+    network_name=$(get_tmux_option "@tmux2k-bandwidth-network-name" "en0")
 elif [[ $(uname -s) == "Linux" ]]; then
     default_network_device="wlo1"
     if command -v ip >/dev/null; then
@@ -51,15 +51,18 @@ main() {
 
     # Do calculations if this isn't the first run
     if [ "$previous_timestamp" != '0' ]; then
-        total_download_bytes=$(expr "$current_rx_bytes" - "$previous_rx_bytes")
-        total_upload_bytes=$(expr "$current_tx_bytes" - "$previous_tx_bytes")
-        seconds=$(expr "$current_timestamp" - "$previous_timestamp")
-        # Avoid division by zero if the plugin runs multiple times within the same second
-        total_download_bytes=$(( current_rx_bytes - previous_rx_bytes ))
-        total_upload_bytes=$(( current_tx_bytes - previous_tx_bytes ))
-        seconds=$(( current_timestamp - previous_timestamp ))
-        total_download_bps=$(( total_download_bytes / seconds ))
-        total_upload_bps=$(( total_upload_bytes / seconds ))
+        total_download_bytes=$(("$current_rx_bytes" - "$previous_rx_bytes"))
+        total_upload_bytes=$(("$current_tx_bytes" - "$previous_tx_bytes"))
+        seconds=$(("$current_timestamp" - "$previous_timestamp"))
+        if [ "$seconds" -eq 0 ]; then
+            seconds=1
+        fi
+
+        total_download_bytes=$((current_rx_bytes - previous_rx_bytes))
+        total_upload_bytes=$((current_tx_bytes - previous_tx_bytes))
+        seconds=$((current_timestamp - previous_timestamp))
+        total_download_bps=$((total_download_bytes / seconds))
+        total_upload_bps=$((total_upload_bytes / seconds))
         output_download=$(get_output_rate "$total_download_bps")
         output_upload=$(get_output_rate "$total_upload_bps")
     fi
