bd5bf14d0f167da9403eaff9e3c4a198c86f5f9d
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-03-11 18:29:15 +0530

    feat: add gradient support to gpu plugin

--- plugins/gpu.sh
+++ plugins/gpu.sh
@@ -5,6 +5,13 @@ export LC_ALL=en_US.UTF-8
 current_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$current_dir/../lib/utils.sh"
 
+gpu_gradient="$(get_tmux_option '@tmux2k-gpu-gradient' '')"
+
+[ -n "$gpu_gradient" ] &&
+    source "$current_dir/../lib/color-utils.sh"
+
+gpu_icon_link_to="$(get_tmux_option '@tmux2k-gpu-icon-link-to' '')"
+
 get_platform() {
     case $(uname -s) in
     Linux)
@@ -17,19 +24,42 @@ get_platform() {
 }
 
 get_gpu() {
+    local gpu
     gpu=$(get_platform)
     if [[ "$gpu" == NVIDIA ]]; then
-        usage=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ sum += $0 } END { printf("%s%%\n", sum / NR) }')
+        local usage
+        usage=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{ sum += $0 } END { printf("%s", sum / NR) }')
+        local output=''
+        if [ -n "$gpu_gradient" ]; then
+            local color
+            color="$(pct2color "${usage}%" "$gpu_gradient")"
+            output+="#[fg=${color:-default}]"
+            [ "$gpu_icon_link_to" = 'usage' ] &&
+                tmux set -g '@tmux2k-gpu-linked-color' "$color"
+        fi
+        output+="$(normalize_padding "${usage}%")"
+        printf '%s' "$output"
     else
-        usage='N/A'
+        normalize_padding 'N/A'
     fi
-    normalize_padding "$usage"
 }
 
 main() {
+    local gpu_icon gpu_usage output=''
     gpu_icon=$(get_tmux_option "@tmux2k-gpu-icon" "")
     gpu_usage=$(get_gpu)
-    echo "$gpu_icon $gpu_usage"
+
+    if [ -z "$gpu_icon_link_to" ] || [ -z "$gpu_gradient" ]; then
+        tmux set -g '@tmux2k-gpu-linked-color' ''
+    else
+        local gpu_linked_color
+        gpu_linked_color="$(get_tmux_option '@tmux2k-gpu-linked-color' '')"
+        [ -n "$gpu_linked_color" ] &&
+            output+="#[fg=${gpu_linked_color}]"
+    fi
+
+    output+="$gpu_icon $gpu_usage"
+    printf '%s' "$output"
 }
 
 main
