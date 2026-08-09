5c773e597af44dad270a51ae0987f33ae97aa2bc
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-08-09 21:45:50 +0530

    refactor: simplify get_tmux_option using printf and parameter expansion

--- lib/utils.sh
+++ lib/utils.sh
@@ -51,15 +51,11 @@ fi
 
 
 get_tmux_option() {
-    local option=$1
-    local default_value=$2
+    local option="$1"
+    local default_value="$2"
     local option_value
     option_value=$(tmux show-option -gqv "$option")
-    if [ -z "$option_value" ]; then
-        echo "$default_value"
-    else
-        echo "$option_value"
-    fi
+    printf '%s\n' "${option_value:-$default_value}"
 }
 
 # Persistent per-plugin state, stored in files rather than tmux options.
